13c8a4007dc1ffc52ff1928c3898aa48129e55a2
NomanKhan3701 <noman.khan3701@gmail.com>
2022-08-09 16:31:14 +0530

    Added the root files for theme selection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import "../public/assets/css/variablesAndThemes/root.scss";
+import "../public/assets/css/variablesAndThemes/theme.scss";
+import "../public/assets/css/globals.scss";
+import "../public/assets/css/utils.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
