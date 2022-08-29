371930e7b1dbd6e961b2c869d35ddba2cae825f7
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-08-29 17:05:42 +0530

    built the login page, just frontend part, have hardcoded the colors and sizes havent used the global variables yet init, need few changes, work in progress

---
+++ pages/login/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./Login.module.scss";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { BsApple, BsGithub } from "react-icons/bs";
+import { GrLinkedinOption } from "react-icons/gr";
+
+const Login = () => {
+  return (
+    <div className={styles.Main_container + " " + "container"}>
+      <div className={styles.left_container}></div>
+      <div className={styles.right_container}>
+        <div className={styles.login_container}>
+          <h3>Welcome Back!👏</h3>
+          <h2>Login to your account</h2>
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Enter email" />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter password" />
+          <button>Login</button>
+          <Link href="/">
+            <a className={styles.link}>Forgot Password</a>
+          </Link>
+          <div className={styles.hr_container}>
+            <hr />
+            Or
+            <hr />
+          </div>
+          <div>
+            <label>Login using</label>
+            <div className={styles.icons_container}>
+              <FcGoogle className={styles.icons} size={"3.5rem"} />
+              <BsApple className={styles.icons} size={"3.5rem"} />
+              <GrLinkedinOption className={styles.icons} size={"3.5rem"} />
+              <BsGithub className={styles.icons} size={"3.5rem"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/UI/LimitChar/LimitChar.jsx
+++ src/components/UI/LimitChar/LimitChar.jsx
@@ -1,16 +1,38 @@
 import React from "react";
-import classes from "./LimitChar.module.scss"
+import classes from "./LimitChar.module.scss";
 
-const LimitChar = ({ word, limit, hoverhide, className, styles, fitContent }) => {
-	return (
-		/* Send hover false or value except true to disable the hover functionality */
-		<div className={classes.limit_char_container + " " + `${className ? className : " " + " " + `${fitContent ? classes.noMax : ""}`}` + " " + `${hoverhide ? "" : word?.length > limit ? classes.limit_char_hover : ""}`} styles={{ ...styles }}>
-			{word && word?.length > limit
-				? word.substring(0, limit).replaceAll("_", " ") + "..."
-				: word?.replaceAll("_", " ")}
-			<div className={classes.full_word_show}>{word}</div>
-		</div>
-	);
+const LimitChar = ({
+  word,
+  limit,
+  hoverhide,
+  className,
+  styles,
+  fitContent,
+}) => {
+  return (
+    /* Send hover false or value except true to disable the hover functionality */
+    <div
+      className={
+        classes.limit_char_container +
+        " " +
+        `${
+          className
+            ? className
+            : " " + " " + `${fitContent ? classes.noMax : ""}`
+        }` +
+        " " +
+        `${
+          hoverhide ? "" : word?.length > limit ? classes.limit_char_hover : ""
+        }`
+      }
+      styles={{ ...styles }}
+    >
+      {word && word?.length > limit
+        ? word.substring(0, limit).replace("_", " ") + "..."
+        : word?.replace("_", " ")}
+      <div className={classes.full_word_show}>{word}</div>
+    </div>
+  );
 };
 
 export default LimitChar;
