1d4eb0135a784d349ce9a133932ecf7f6f603bed
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-09-18 12:35:37 +0530

    testing the deploymeny and authentication working or not,

--- pages/create-blog/CustomEditor.js
+++ pages/create-blog/CustomEditor.js
@@ -1,5 +1,5 @@
 const CustomEditor = ({ data, imageArray, handleInstance }) => {
-  return <p>sorry noman didnt find any solution for the editorJs so</p>;
+  return <p>sorry noman didnt find any solution for the editorJs so,</p>;
 };
 
 export default CustomEditor;
